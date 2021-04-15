111e8a9f3bd2eba0eaab78c9a3516c948a51ecc3
Allexandere <39259989+Allexandere@users.noreply.github.com>
2021-04-15 04:57:02 +0300

    moviedb base added

---
+++ moviedb-api-service/src/test/java/io/amras/moviedbapiservice1/MoviedbApiService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.amras.moviedbapiservice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviedbApiService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
